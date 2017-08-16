457ad651d75454e9d7929816804e123b158a002e
zdajani <dajani.zeina@gmail.com>
2017-08-16 13:40:55 -0400

    set up app

---
+++ lib/tasks/start.rake
@@ -0,0 +1,17 @@
+# Call rake.start instead of having to call foreman start -f Procfile.dev 
+# to start dev server 
+
+namespace :start do
+  task :development do
+    exec 'foreman start -f Procfile.dev'
+  end
+  
+  desc 'Start production server'
+  task :production do
+    exec 'NPM_CONFIG_PRODUCTION=true yarn postinstall && foreman start'
+  end
+end
+
+# default to dev server on rake.start
+desc 'Start development server'
+task :start => 'start:development'
---
+++ spec/rails_helper.rb
@@ -0,0 +1,42 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+#requires all files in support folder
+Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  
+  # Includes factory girl methods to tests
+  config.include FactoryGirl::Syntax::Methods
+  
+  # includes api method helpers 
+  config.include ApiHelper, type: :api
+  
+  # includes json parser for tests
+  config.include Requests::JsonHelpers, type: :request
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
+
+# Shoulda matcher config
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+  end
+end
